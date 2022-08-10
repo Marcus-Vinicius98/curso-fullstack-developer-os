b7e7852cb9b2c2119d04777dfacd242767a053a0
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-08-10 00:58:47 -0300

    Classe cliente

---
+++ src/main/java/com/api/os/domain/Cliente.java
@@ -0,0 +1,16 @@
+package com.api.os.domain;
+
+public class Cliente extends Pessoa{
+
+	public Cliente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cliente(Long id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
