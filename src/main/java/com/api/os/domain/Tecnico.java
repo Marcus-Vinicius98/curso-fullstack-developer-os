47f2d9d4c31d3353552bd19cd75a5fd1d4fb7b89
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-08-10 00:56:46 -0300

    Classe tecnico

---
+++ src/main/java/com/api/os/domain/Tecnico.java
@@ -0,0 +1,17 @@
+package com.api.os.domain;
+
+public class Tecnico extends Pessoa{
+
+	public Tecnico() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Tecnico(Long id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
