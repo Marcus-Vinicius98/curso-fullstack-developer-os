9002966967e03a3fbe71a448be86e143990ef732
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-08-11 00:52:31 -0300

    Melhoria no perfil de testes

--- src/main/java/com/api/os/services/DBService.java
+++ src/main/java/com/api/os/services/DBService.java
@@ -1,12 +1,10 @@
-package com.api.os.config;
+package com.api.os.services;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 
 import com.api.os.domain.Cliente;
 import com.api.os.domain.OrdemServico;
@@ -17,30 +15,29 @@ import com.api.os.repositories.ClienteRepository;
 import com.api.os.repositories.OrdemServicoRepository;
 import com.api.os.repositories.TecnicoRepository;
 
-@Configuration
-public class LocalConfig {
-	
+@Service
+public class DBService {
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private OrdemServicoRepository ordemServicoRepository;
-	
+
 	@Autowired
 	private TecnicoRepository tecnicoRepository;
+
 	
-	@Bean
-	public void StartDb() {
-		
-		Cliente c1 = new Cliente(null, "marcus","679.122.690-02", "85 85745-7482");
+	public void instanciaDB() {
+
+		Cliente c1 = new Cliente(null, "marcus", "679.122.690-02", "85 85745-7482");
 		Tecnico t1 = new Tecnico(null, "andreia", "611.778.100-85", "85 85745-7482");
-		OrdemServico os1 = new OrdemServico(null,Prioridade.ALTA, "Teste create", Status.ANDAMENTO, t1, c1);
-		
+		OrdemServico os1 = new OrdemServico(null, Prioridade.ALTA, "Teste create", Status.ANDAMENTO, t1, c1);
+
 		clienteRepository.saveAll(Arrays.asList(c1));
 		tecnicoRepository.saveAll(Arrays.asList(t1));
 		ordemServicoRepository.saveAll(Arrays.asList(os1));
-		
-		
+
 	}
 
 }
